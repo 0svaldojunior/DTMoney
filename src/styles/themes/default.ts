ad2a4631b5a6be8c870739703484f92c5aa8903f
0svaldojunior <app.o.r.jr@gmail.com>
2023-04-25 00:24:43 -0300

    visual finished

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  gray100: '#E1E1E6',
+  gray300: '#C4C4CC',
+  gray400: '#8D8D99',
+  gray500: '#7C7C8A',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  green300: '#00B37E',
+  green500: '#00875F',
+  green700: '#015F43',
+
+  red300: '#F75A68',
+  red500: '#AA2834',
+
+  yellow500: '#FBA94C',
+} as const
